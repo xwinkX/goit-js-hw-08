79ce423112d12bf43d7f8f301bc304a323edc081
xwinkX <opalyuk.e88@ukr.net>
2022-05-28 16:20:16 +0300

    update2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+const player = new Player('vimeo-player');
 
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(
+  JSON.parse(localStorage.getItem('videoplayer-current-time'))
+);
