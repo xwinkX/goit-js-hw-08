5b2c77b5b8f1612b35dcd5c8428a094a0f09d0fa
xwinkX <opalyuk.e88@ukr.net>
2022-05-28 09:13:45 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+const imageGallery = document.querySelector('.gallery');
+const galleryMarkup = createGallery(galleryItems);
+imageGallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGallery(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+            <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 console.log(galleryItems);
