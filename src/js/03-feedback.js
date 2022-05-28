c1ef694e6680905031bd29ebd486fde00c06825d
xwinkX <opalyuk.e88@ukr.net>
2022-05-28 22:23:14 +0300

    finish

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
 
+const MESSAGE_KEY = 'local-value';
+const formData = {};
+
+const formEl = document.querySelector('.feedback-form');
+const inputEl = document.querySelector('.feedback-form textarea');
+const emailEl = document.querySelector('.feedback-form input');
+formEl.addEventListener('submit', onFormSubmit);
+formEl.addEventListener('input', throttle(element, 500));
+
+function element(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(MESSAGE_KEY, JSON.stringify(formData));
+}
+
+valueTextarea();
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem(MESSAGE_KEY);
+  console.log(formData);
+}
+
+function valueTextarea() {
+  const savedMessage = JSON.parse(localStorage.getItem(MESSAGE_KEY));
+  if (savedMessage) {
+    emailEl.value = savedMessage.email;
+    inputEl.value = savedMessage.message;
+  }
+}
